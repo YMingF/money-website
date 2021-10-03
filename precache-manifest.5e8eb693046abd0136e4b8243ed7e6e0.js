c7c0df97aa66239dba70b8752d599cde43c1aefd
YM <a834319313@163.con>
2021-10-03 20:41:48 +0800

    deploy

---
+++ precache-manifest.5e8eb693046abd0136e4b8243ed7e6e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f235dff5722d89e7457",
+    "url": "/money-website/css/app.223d1767.css"
+  },
+  {
+    "revision": "b5c067df7e7c6704b8bb793173d6b971",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "5f235dff5722d89e7457",
+    "url": "/money-website/js/app.439c751a.js"
+  },
+  {
+    "revision": "ff26d97c056784387089",
+    "url": "/money-website/js/chunk-vendors.e48ceae0.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "d085e8d912d6c91625fe9672565f6301",
+    "url": "/money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
